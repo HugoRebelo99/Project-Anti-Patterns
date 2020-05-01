319e3098f3e240645da3d33abc1ed8f0ab6cf894
Sworks99 <ricky.tomas99@gmail.com>
2020-05-01 13:04:41 +0100

    Reviewers code

--- tests/Testes.java
+++ tests/Testes.java
@@ -11,17 +11,12 @@ public class Testes {
 
     @BeforeAll
     public static void setup() throws Exception {
-        assertThrows(PessoaNotValidatedException.class, () -> {
-            pessoa = new Pessoa("Ricardo", -1, "Esquina à direita como de quem vem da esquerda", "123456789");
-        });
-
-        assertDoesNotThrow(() -> {
-            pessoa = new Pessoa("Ricardo", 20, "Esquina à direita como de quem vem da esquerda", "123456789");
-        });
-        System.out.println("Uma nova pessoa foi criada.");
+        pessoa = new Pessoa("Ricardo", 20, "Esquina à direita como de quem vem da esquerda", "123456789");
+        //System.out.println("Uma nova pessoa foi criada.");
     }
 
-    @Test
+    //loudmouth, enumerator,anal probe,free ride
+   /* @Test
     public void testPessoa1() throws NoSuchFieldException, IllegalAccessException {
 
         System.out.println("Teste que verifica o nome e a idade.");
@@ -34,21 +29,32 @@ public class Testes {
         int Idade = (int) getIdade.get(pessoa);
         assertEquals(21, Idade);
         System.out.println("Idade: " + Idade);
+    }*/
+
+    @Test
+    public void testPessoaNome() throws NoSuchFieldException, IllegalAccessException {
+        assertEquals("Ricardo", pessoa.getNome());
     }
 
     @Test
-    public void testPessoa2(){
+    public void testPessoaIdade() throws NoSuchFieldException, IllegalAccessException {
+        assertEquals(21, pessoa.getIdade());
+    }
+
+    @Test
+    public void testPessoaContribuinte(){
         assertEquals("123456789", pessoa.getContribuinte());
     }
 
-    @Test
+    @Test //cucko e loudmouth
     public void testPessoaMorada() throws Exception {
-        Pessoa p = new Pessoa("Hugo", 20, "Ali debaixo da ponte", "987654321");
+        /*Pessoa p = new Pessoa("Hugo", 20, "Ali debaixo da ponte", "987654321");
         assertEquals("Ali debaixo da ponte", p.getMorada());
-        System.out.println("Morada: " + p.getMorada());
+        System.out.println("Morada: " + p.getMorada());*/
+        assertEquals("Esquina à direita como de quem vem da esquerda", pessoa.getMorada());
     }
-
-    @Test
+    //slow poke
+  /*  @Test
     public void largeDataSimulation() throws Exception {
         ArrayList<Integer> Data = new ArrayList<Integer>();
         for(int i = 0; i < 100; i++){
@@ -59,6 +65,22 @@ public class Testes {
             Pessoa pessoa = new Pessoa("David", 22, "", "123459876");
         });
 
+    }*/
+    @Test
+    public void testPessoaException() throws Exception {
+        assertThrows(PessoaNotValidatedException.class, () ->{
+            Pessoa pessoa = new Pessoa("David", 22, "", "123459876");
+        });
+
     }
+
+    @Test
+    public void testPessoaidadeException() throws Exception {
+        assertThrows(PessoaNotValidatedException.class, () -> {
+            pessoa = new Pessoa("Ricardo", -1, "Esquina à direita como de quem vem da esquerda", "123456789");
+        });
+    }
+
+
 
 }
